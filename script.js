6884510b72efca4120f38aa44c80171ceeae3651
Jaki Hawker <jmichel910@yahoo.com>
2021-11-06 19:21:21 -0600

    added finishing code

--- script.js
+++ script.js
@@ -15,14 +15,13 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
 
-
 // An empty array made to store the input provided by the user
 var passwordPool = [];
 
 // Function declaration
 function passwordGen() {
 
-  // Prompting user to select a number between 8 - 128
+  // Prompting the user to select a number between 8 - 128
   var userNumber = parseInt(prompt("Choose a number between 8 - 128 to generate your password!"));
 
   // User will be alerted to "try again" if they select a number outside 8 and 128 
@@ -30,11 +29,12 @@ function passwordGen() {
     alert("The value is invalid. Please try again."); return passwordGen();
   }
 
-// Confirm passwordpool characters
-var wantLower = confirm("Click OK to include lowercase letters.");
-var wantUpper = confirm("Click OK to include uppercase letters.");
-var wantNumber = confirm("Click OK to include numbers.");
-var wantSpecial = confirm("Click OK to include special characters.");
+  // Confirm passwordpool characters
+  var wantLower = confirm("Are lowercase letters OK?");
+  var wantUpper = confirm("Are uppercase letters OK?");
+  var wantNumber = confirm("Are numbers OK?");
+  var wantSpecial = confirm("Are special characters OK?");
+
 
   // Conditions of user selections
   if (wantUpper) {
@@ -53,15 +53,36 @@ var wantSpecial = confirm("Click OK to include special characters.");
   }
 
 
+  // ERROR CONDITION - If all values equal false the user will be alerted to choose a criteria and be returned to the beginning of the function
+  if (wantUpper == false && wantLower == false && wantNumber == false && wantSpecial == false) {
+    alert("Please enter a password criteria.");
+    return passwordGen();
+  }
 
+ // For loop will go through all arrays requested pushed to passwordPool
+ var result = [];
+  for (var i = 0; i < userNumber; i++) {
+    result.push(passwordPool[Math.floor(Math.random() * passwordPool.length)]);
+  }
+
+ // Error condition to validate that each password has random items from user input
+ if (wantLower) { result[2] = lowerCase[Math.floor(Math.random() * lowerCase.length)] }
+  if (wantUpper) { result[9] = upperCase[Math.floor(Math.random() * upperCase.length)] }
+  if (wantNumber) { result[6] = number[Math.floor(Math.random() * number.length)] }
+  if (wantSpecial) { result[3] = specialChar[Math.floor(Math.random() * specialChar.length)] }
+
+
+  // Turn result array into a string
+  return result.join("");
+
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = passwordGen();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
