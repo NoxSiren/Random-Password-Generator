6c1b756e37d5f07be1ae74548d9d1a363950ed28
Jaki Hawker <jmichel910@yahoo.com>
2021-11-06 18:33:44 -0600

    variables added

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//create prompts to determine password criteria
+//user can respond with a true or false
+//based on user response determins possible characters for password
+//check length input to ensure at least 8 chr no more than 128 chr
+//check to see that one prompt was true
+//after all prompts have been answered generate a random password
+//ensure generate password function has a return value
+
+// Variables
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,15 +32,6 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//create prompts to determine password criteria
-//user can respond with a true or false
-//based on user response determins possible characters for password
-//check length input to ensure at least 8 chr no more than 128 chr
-//check to see that one prompt was true
-//after all prompts have been answered generate a random password
-//ensure generate password function has a return value
-
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9','0']
 
 function generatePassword(){
   var result = confirm("Would you like numbers?");
@@ -32,8 +41,6 @@ function generatePassword(){
     console.log(passChar)
   }
 }
-
-var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 function generatePassword(){
   var result = confirm("Would you like lower case letters?");
